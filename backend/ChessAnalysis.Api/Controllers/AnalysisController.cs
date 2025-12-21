a7685ed4e8fa15e74209693f4945893fad1031b7
Nicholas Facciola <nicholas.facciola@gmail.com>
2025-12-20 20:52:29 -0800

    convert .NET project to web api mvs

---
+++ backend/ChessAnalysis.Api/Controllers/AnalysisController.cs
@@ -0,0 +1,23 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace ChessAnalysis.Api.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class AnalysisController : ControllerBase
+{
+    [HttpPost("upload")]
+    public async Task<IActionResult> UploadPgn(IFormFile pgnFile)
+    {
+        if (pgnFile == null || pgnFile.Length == 0)
+        {
+            return BadRequest("No pgn file uploaded");
+        }
+
+        return Ok(new
+        {
+            fileName = pgnFile.FileName,
+            size = pgnFile.Length
+        });
+    }
+}
